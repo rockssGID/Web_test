f9c33a61d807fdb28fac499ba46019f4e1ac364c
rockssGID <rockssaa@icloud.com>
2025-01-31 17:51:32 +0300

    modified:   css/header_footer_menu_menu_burger.css 	modified:   css/main.css 	modified:   css/responsive/main.css 	modified:   index.html 	modified:   js/swiper_settings.js

--- js/swiper_settings.js
+++ js/swiper_settings.js
@@ -1,28 +1,38 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.swiper_contener', {
     direction: 'horizontal',
     effect: 'coverflow',
     loop: true,
-    spaceBetween: 20,
+    spaceBetween: -150,
     slidesPerView: 3,
     mousewheel: true,
-	centeredSlides: true,
+	  centeredSlides: true,
     pagination: {
         el: '.swiper-pagination',
         dynamicBullets: true,
 		clickable: true,
       },
-/* 
+
       autoplay: {
-        delay: 1000,
+        delay: 3000,
         disableOnInteraction: false,
-      }, */
+      },
   
-      coverflowEffect: {
+  coverflowEffect: {
 		rotate: 0,
-		stretch: 0,
-		depth: 0,
-		modifier: 1,
+		stretch: -50,
+		depth: 100,
+		modifier: 1.5,
 		slideShadows: true,
 	},
-  
+  breakpoints: {
+    320: {
+      slidesPerView: 1.3,
+    },
+    600: {
+      slidesPerView: 2.6,
+    },
+    1024: {
+      slidesPerView: 2.9,
+    }
+}
   });
