a7edb6f03528e012992186a2992ee8ead9a3c746
rockssGID <rockssaa@icloud.com>
2025-02-02 15:44:31 +0300

    deleted:    css/extra/swiper.min.css 	modified:   css/main.css 	modified:   css/responsive/main.css 	deleted:    images/botton.png 	deleted:    images/lif.png 	deleted:    images/ria.png 	new file:   images/stock.png 	modified:   index.html 	deleted:    js/swiper.min.js 	modified:   js/swiper_settings.js

--- js/swiper_settings.js
+++ js/swiper_settings.js
@@ -1,9 +1,9 @@
-const swiper = new Swiper('.swiper_contener', {
+/* const swiper = new Swiper('.swiper_contener, .swiper_stock', {
     direction: 'horizontal',
     effect: 'coverflow',
     loop: true,
     spaceBetween: -150,
-    slidesPerView: 3,
+    slidesPerView: 'auto',
     mousewheel: true,
 	  centeredSlides: true,
     pagination: {
@@ -12,12 +12,12 @@ const swiper = new Swiper('.swiper_contener', {
 		clickable: true,
       },
 
-      autoplay: {
+/*       autoplay: {
         delay: 3000,
         disableOnInteraction: false,
-      },
+      }, */
   
-  coverflowEffect: {
+/*   coverflowEffect: {
 		rotate: 0,
 		stretch: -50,
 		depth: 100,
@@ -35,4 +35,44 @@ const swiper = new Swiper('.swiper_contener', {
       slidesPerView: 2.9,
     }
 }
-  });
+  }); */
+
+var swiper = new Swiper(".swiper_contener", {
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: -150,
+  slidesPerView: 'auto',
+  mousewheel: true,
+  centeredSlides: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.3,
+    },
+    600: {
+      slidesPerView: 2.6,
+    },
+    1024: {
+      slidesPerView: 2.9,
+    }
+  }
+    
+})
+var swiper2 = new Swiper(".swiper_stock", {
+  direction: 'horizontal',
+  loop: true,
+  spaceBetween: 50,
+  slidesPerView: 3,
+  mousewheel: true,
+  centeredSlides: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.3,
+    },
+    600: {
+      slidesPerView: 2.6,
+    },
+    1024: {
+      slidesPerView: 2.9,
+    }
+  }
+})
