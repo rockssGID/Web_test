a0492bf5486623156eb33609c4e059ad2e4f14ec
rockssGID <rockssaa@icloud.com>
2025-02-04 13:15:38 +0300

    modified:   css/header_footer_menu_menu_burger.css 	modified:   index.html 	modified:   js/swiper_settings.js	modified:   scss/_extra.scss 	modified:   scss/main.css 	modified:   scss/main.css.map	modified:   scss/main.scss

--- js/swiper_settings.js
+++ js/swiper_settings.js
@@ -1,46 +1,8 @@
-/* const swiper = new Swiper('.swiper_contener, .swiper_stock', {
-    direction: 'horizontal',
-    effect: 'coverflow',
-    loop: true,
-    spaceBetween: -150,
-    slidesPerView: 'auto',
-    mousewheel: true,
-	  centeredSlides: true,
-    pagination: {
-        el: '.swiper-pagination',
-        dynamicBullets: true,
-		clickable: true,
-      },
-
-/*       autoplay: {
-        delay: 3000,
-        disableOnInteraction: false,
-      }, */
-  
-/*   coverflowEffect: {
-		rotate: 0,
-		stretch: -50,
-		depth: 100,
-		modifier: 1.5,
-		slideShadows: true,
-	},
-  breakpoints: {
-    320: {
-      slidesPerView: 1.3,
-    },
-    600: {
-      slidesPerView: 2.6,
-    },
-    1024: {
-      slidesPerView: 2.9,
-    }
-}
-  }); */
 
 var swiper = new Swiper(".swiper_contener", {
   direction: 'horizontal',
   loop: true,
-  spaceBetween: -90,
+  spaceBetween: 50,
   slidesPerView: '3',
   /* mousewheel: true, */
   centeredSlides: true,
@@ -48,7 +10,7 @@ var swiper = new Swiper(".swiper_contener", {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-  breakpoints: {
+/*   breakpoints: {
     320: {
       slidesPerView: 1.3,
     },
@@ -58,25 +20,6 @@ var swiper = new Swiper(".swiper_contener", {
     1024: {
       slidesPerView: 2.9,
     }
-  }
+  } */
     
-})
-var swiper2 = new Swiper(".swiper_stock", {
-  direction: 'horizontal',
-  loop: true,
-  spaceBetween: 50,
-  slidesPerView: 3,
-  mousewheel: true,
-  centeredSlides: true,
-  breakpoints: {
-    320: {
-      slidesPerView: 1.3,
-    },
-    600: {
-      slidesPerView: 2.6,
-    },
-    1024: {
-      slidesPerView: 2.9,
-    }
-  }
 })
