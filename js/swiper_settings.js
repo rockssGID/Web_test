293ec6305fe7687baec1b1953a130f2b9801b13b
rockssGID <rockssaa@icloud.com>
2025-02-04 04:25:03 +0300

    modified:   css/header_footer_menu_menu_burger.css 	deleted:    css/main.css 	modified:   css/responsive/main.css 	modified:   images/header_footer/telegram.png 	modified:   index.html 	modified:   js/swiper_settings.js 	new file:   scss/_extra.scss 	new file:   scss/main.css 	new file:   scss/main.css.map 	new file:   scss/main.scss

--- js/swiper_settings.js
+++ js/swiper_settings.js
@@ -40,10 +40,14 @@
 var swiper = new Swiper(".swiper_contener", {
   direction: 'horizontal',
   loop: true,
-  spaceBetween: -150,
-  slidesPerView: 'auto',
-  mousewheel: true,
+  spaceBetween: -90,
+  slidesPerView: '3',
+  /* mousewheel: true, */
   centeredSlides: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
   breakpoints: {
     320: {
       slidesPerView: 1.3,
